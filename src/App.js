322a0798f716d3a86221d5129fd8beecc8fc8f5c
RAJ <rajumacha@yahoo.com>
2020-08-20 15:35:44 +0530

    folder structure created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Alert from './components/Alert';
+import ExpenseForm from './components/ExpenseForm';
+import ExpenseList from './components/ExpenseList';
+import { v4 as uuid } from 'uuid';
+
+
+const initialExpenses = [
+  { id: uuid(), chanrge: "rent", amount: 1600 },
+  { id: uuid(), chanrge: "car payment", amount: 600 },
+  { id: uuid(), chanrge: "credit card payment", amount: 1200 }
+]
 
 function App() {
+  const result = useState(initialExpenses);
+  const expenses = result[0]
+  const setExpenses = result[1]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Alert />
+      <h1>budget calculator</h1>
+      <main className="App">
+        <ExpenseForm />
+        <ExpenseList expenses={expenses} />
+      </main>
+      <h1>total spending : <span className="total">
+        ${
+          expenses.reduce((acc, item) => {
+            return acc += item.amount
+          }, 0)
+        }
+      </span>
+      </h1>
+    </>
   );
 }
 
